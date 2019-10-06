a3d0d1adc292ece00c3686f8b85435c5659e2358
Sicking <sicking@ernstings-family.com>
2019-10-06 10:59:12 +0200

    first commit

---
+++ NotePadPlusPlus.ps1
@@ -0,0 +1,24 @@
+function get-NPPCurrentVersion {
+    [cmdletbinding()]
+    [outputtype([version])]
+    $Url = "https://notepad-plus-plus.org/downloads/"
+    $Content = Invoke-WebRequest -Uri $url
+    $Matches = ($content.allelements | ? tagName -eq 'a').innerText -match "Current Version \d+\.\d+\.\d+"
+    $ver = [version]::new($Matches[0].TrimStart("Current Version "))
+    Write-Output $ver
+}
+
+function get-NPPCurrentDownloadURL {
+    [cmdletbinding()]
+    [outputtype([string])]
+    param (
+        [validateSet("x86","x64")][string]$Architecture = "x64"
+    )
+    $version = get-NPPCurrentVersion
+    if ("x86" -eq $Architecture) { $archcode = "" } else { $archcode = ".x64" }
+    $url = "http://download.notepad-plus-plus.org/repository/$($version.major).x/$version/npp.$($version).Installer$($archcode).exe"
+    Write-Output $url
+}
+#Example get-NPPCurrentDownloadURL -Architecture x64
+
+#Example wget -uri (get-NPPCurrentDownloadURL) -OutFile .\npp.exe
